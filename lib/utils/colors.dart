5a1590bb15021fe087bec6c5e7724dcaf8dae7e8
Jeremy Chong <h09083.cys@gmail.com>
2022-12-18 19:12:41 +0800

    Update App Theme Colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
-const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const mobileBackgroundColor = Color.fromRGBO(47, 47, 47, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
-const blueColor = Color.fromRGBO(0, 149, 246, 1);
+
+const orange1Color = Color.fromRGBO(255, 88, 0, 1);
+const orange2Color = Color.fromRGBO(255, 155, 104, 1);
+const orange3Color = Color.fromRGBO(255, 222, 205, 1);
+
+const red1Color = Color.fromRGBO(255, 0, 0, 1);
+const red2Color = Color.fromRGBO(255, 104, 104, 1);
+const red3Color = Color.fromRGBO(255, 205, 205, 1);
+
+const green1Color = Color.fromRGBO(47, 108, 84, 1);
+const green2Color = Color.fromRGBO(74, 154, 122, 1);
+const green3Color = Color.fromRGBO(210, 235, 219, 1);
+
+const blue1Color = Color.fromRGBO(47, 79, 108, 1);
+const blue2Color = Color.fromRGBO(74, 116, 154, 1);
+const blue3Color = Color.fromRGBO(210, 223, 235, 1);
+
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
