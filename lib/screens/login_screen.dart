1aeebf6c5baa8adfafa93b6d61c38fa281043c4e
Jeremy Chong <h09083.cys@gmail.com>
2022-12-18 19:34:31 +0800

    Update Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:foodport_app/utils/colors.dart';
-import 'package:foodport_app/widgets/text_field_input.dart';
+
+import '../utils/colors.dart';
+import '../widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
