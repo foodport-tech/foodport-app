7e334c805ad0f111816d9e44b0edc70461f784f4
Jeremy Chong <h09083.cys@gmail.com>
2022-12-18 17:49:46 +0800

    Setup Firebase.initializeApp()

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../responsive/mobile_screen_layout.dart';
 import '../responsive/responsive_layout_screen.dart';
@@ -5,7 +7,21 @@ import '../responsive/tablet_screen_layout.dart';
 import '../responsive/web_screen_layout.dart';
 import '../utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyABzu27DTyMD7Pb2jcYfiYbfENtpPZeR7Q",
+        appId: "1:975078029701:web:0a8e0e9bf0b301eb0e6cec",
+        messagingSenderId: "975078029701",
+        projectId: "foodport-app-183e3",
+        storageBucket: "foodport-app-183e3.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
