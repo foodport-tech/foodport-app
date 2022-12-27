4acb899f4787354985058db68b28879eec373ea5
Jeremy Chong <h09083.cys@gmail.com>
2022-12-27 10:51:13 +0800

    Complete Youtube Tutorial

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -104,4 +104,9 @@ class AuthMethods {
 
     return res;
   }
+
+  // Sign Out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
 }
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,19 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/feed_post_screen.dart';
 import '../screens/create_post_screen.dart';
 import '../screens/feed_post_screen_ig.dart';
+import '../screens/profile_screen_ig.dart';
+import '../screens/search_screen_ig.dart';
 
 const tabletScreenSize = 576;
 const webScreenSize = 992;
 
 // Display different screens for different bottom bar button in mobile_screen_layout.dart
-const homeScreenItems = [
+List<Widget> homeScreenItems = [
   // FeedPostScreenIG(),
-  FeedPostScreen(),
+  const FeedPostScreen(),
   // For Screen 3: Create Post
-  Center(child: Text('explore')),
-  AddPostScreen(),
-  Center(child: Text('inbox')),
-  Center(child: Text('profile')),
+  const SearchScreenIG(),
+  //Center(child: Text('explore')),
+  const AddPostScreen(),
+  const Center(child: Text('inbox')),
+  ProfileScreenIG(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
