0df5d445a00a8c1d87d64224db4ff15e36dfa5a0
Jeremy Chong <h09083.cys@gmail.com>
2022-12-28 21:38:26 +0800

    Extract widget 'post_card_side_bar.dart'

--- lib/widgets/post_card/post_card.dart
+++ lib/widgets/post_card/post_card.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../models/post.dart';
-import '../../screens/dish_detail_screen.dart';
+import '../../providers/post.dart';
 import '../../utils/colors.dart';
 import '../like_animation.dart';
+import 'post_card_side_bar.dart';
 
 class PostCard extends StatefulWidget {
   // TEST Listener posts[index]
@@ -46,7 +46,7 @@ class _PostCardState extends State<PostCard> {
 
   @override
   Widget build(BuildContext context) {
-    final post = Provider.of<Post>(context);
+    final post = Provider.of<Post>(context, listen: false);
 
     return Padding(
       padding: const EdgeInsets.only(
@@ -275,89 +275,7 @@ class _PostCardState extends State<PostCard> {
                     ),
 
                     // Like, Comment, Share Section
-                    Container(
-                      alignment: Alignment.topCenter,
-                      child: Column(
-                        children: [
-                          // Food Menu Section
-                          // TODO: If post tagged dish, then only show this
-                          IconButton(
-                            icon: const Icon(
-                              Icons.menu_book_outlined,
-                              size: 24,
-                            ),
-                            onPressed: () {
-                              // Better method
-                              Navigator.of(context).pushNamed(
-                                DishDetailScreen.routeName,
-                                arguments: post.postId,
-                                // arguments: widget.dishId,
-                              );
-                              // Not a good method:
-                              // Navigator.of(context).push(
-                              //   MaterialPageRoute(
-                              //     builder: (ctx) => DishDetailScreen(),
-                              //   ),
-                              // );
-                            },
-                          ),
-                          const Text("6793"),
-                          const SizedBox(height: 16),
-
-                          // Like Section
-                          LikeAnimation(
-                            isAnimating: true, // Temp
-                            // TOFIX: Firebase
-                            // isAnimating:
-                            //     widget.snap['likes'].contains(user.uid),
-                            smallLike: true,
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                // Temp
-                                Icons.favorite,
-                                size: 24,
-                                color: red2Color,
-                              ),
-                            ),
-                          ),
-
-                          // Display amount of likes
-                          Text('123'), // Temp
-                          // TOFIX: Firebase
-                          // Text("${widget.snap['likes'].length}"),
-                          const SizedBox(height: 16),
-
-                          // Comment Section
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.comment_outlined,
-                              size: 24,
-                            ),
-                          ),
-                          Text('12'), // Temp
-                          // TOFIX: Firebase
-                          // Text("$commentLen"),
-                          const SizedBox(height: 16),
-
-                          // Save Post Section
-                          const Icon(
-                            Icons.bookmark_outline,
-                            size: 24,
-                          ),
-                          const Text("7093"),
-                          const SizedBox(height: 16),
-
-                          // Share Post Section
-                          const Icon(
-                            Icons.share_outlined,
-                            size: 24,
-                          ),
-                          const Text("5604"),
-                        ],
-                      ),
-                    ),
+                    PostCardSideBar(post: post),
                     const SizedBox(
                       width: 16,
                     ),
